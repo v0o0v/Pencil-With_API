2a88908831750775ce63d6d6fdf55deef3b68e96
v0o0v <v0o0v@daum.net>
2021-03-11 11:46:44 +0900

    Initial commit

---
+++ src/test/java/com/pencilwith/apiserver/ApiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pencilwith.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
