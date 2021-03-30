097da7b024b6ba9fdbf705a5e028d69f9e0beaad
kyumin <kyumin.choi@nhnsoft.com>
2021-03-30 22:14:28 +0900

    chore: remove useless library

--- src/main/java/com/pencilwith/apiserver/service/SignUpService.java
+++ src/main/java/com/pencilwith/apiserver/service/SignUpService.java
@@ -1,18 +1,15 @@
 package com.pencilwith.apiserver.service;
 
+import com.pencilwith.apiserver.model.entity.Account;
 import com.pencilwith.apiserver.model.mapper.AccountMapper;
 import com.pencilwith.apiserver.model.request.SignUpRequest;
-import com.pencilwith.apiserver.model.entity.Account;
 import com.pencilwith.apiserver.repository.SignUpRepository;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpService {
-
-    private static ModelMapper modelMapper = new ModelMapper();
 
     private final SignUpRepository signUpRepository;
 
