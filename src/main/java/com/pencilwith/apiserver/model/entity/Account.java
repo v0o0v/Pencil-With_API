7d807537aa59ad11828e5e3864782265b532ea3f
kyumin <kyumin.choi@nhnsoft.com>
2021-03-31 23:34:44 +0900

    feat: add account entity detail setting

--- src/main/java/com/pencilwith/apiserver/model/entity/Account.java
+++ src/main/java/com/pencilwith/apiserver/model/entity/Account.java
@@ -1,11 +1,16 @@
 package com.pencilwith.apiserver.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pencilwith.apiserver.model.enums.Career;
 import com.pencilwith.apiserver.model.enums.Gender;
 import com.pencilwith.apiserver.model.enums.Location;
 import java.time.LocalDate;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,20 +19,24 @@ import lombok.Getter;
 @Getter
 public class Account {
 
-    @Id
-    @GeneratedValue
+    @JsonIgnore
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String nickName;
 
     private String profileImage;
 
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     private LocalDate birth;
 
+    @Enumerated(EnumType.STRING)
     private Location location;
 
+    @Enumerated(EnumType.STRING)
     private Career career;
 
     private String introduction;
