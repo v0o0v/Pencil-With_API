ac95313aa42aac3f0619fcd477fc49439d3b6b0f
kyumin <kyumin.choi@nhnsoft.com>
2021-04-17 22:56:20 +0900

    refactor: change class name

--- src/main/java/com/pencilwith/apiserver/start/config/auth/AuthStorageConfig.java
+++ src/main/java/com/pencilwith/apiserver/start/config/auth/AuthStorageConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AuthConfig {
+public class AuthStorageConfig {
 
     private static final int MAX_SIZE = 1000;
 
