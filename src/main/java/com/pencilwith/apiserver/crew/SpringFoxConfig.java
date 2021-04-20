0ec531f5efc453cfff047ffcef0eba71e1d35225
v0o0v <v0o0v@daum.net>
2021-04-20 19:44:27 +0900

    - Swagger UI에 Auth 설정

--- src/main/java/com/pencilwith/apiserver/crew/SpringFoxConfig.java
+++ src/main/java/com/pencilwith/apiserver/crew/SpringFoxConfig.java
@@ -3,14 +3,19 @@ package com.pencilwith.apiserver.crew;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -19,14 +24,25 @@ public class SpringFoxConfig {
 
     @Bean
     public Docket api() {
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(
+                        List.of(new ParameterBuilder().name("Authorization")
+                                .description("Access Token")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build())
+                )
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
                 .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.pencilwith.apiserver"))
                 .paths(PathSelectors.ant("/**"))
-                .build();
+                .build()
+                .securitySchemes(Arrays.asList(new ApiKey("Bearer +accessToken", "Authorization", "header")))
+                ;
     }
 
     private Set<String> getConsumeContentTypes() {
