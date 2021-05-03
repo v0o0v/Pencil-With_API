47db8a3903eb73d60de62aa70b07e0f61abaecb8
seculoper <mir8725@naver.com>
2021-05-03 21:04:06 +0900

    ProjectService: Transaction 처리

--- src/main/java/com/pencilwith/apiserver/working/service/ProjectService.java
+++ src/main/java/com/pencilwith/apiserver/working/service/ProjectService.java
@@ -11,6 +11,7 @@ import com.pencilwith.apiserver.working.mapper.ProjectMapper;
 import com.pencilwith.apiserver.working.mapper.ProjectTitleMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -20,29 +21,34 @@ import java.util.NoSuchElementException;
 public class ProjectService {
     private final ProjectRepository projectRepository;
 
+    @Transactional(readOnly = true)
     public List<ProjectTitleResponse> selectAllTitles(User user) {
         // 현재 사용자의 프로젝트 리스트 조회
         List<Project> projects = user.getProject();
         return ProjectTitleMapper.toDto(projects);
     }
 
+    @Transactional(readOnly = true)
     public ProjectResponse selectProject(Long id, User user) {
         // 현재 사용자의 프로젝트를 조회함
         Project project = projectRepository.findProjectByOwnerId(user.getId());
         return ProjectMapper.toDto(project);
     }
 
+    @Transactional
     public ProjectResponse createProject(ProjectRequest projectRequest) {
         Project newProject = projectRepository.save(ProjectMapper.toEntity(projectRequest));
         return ProjectMapper.toDto(newProject);
     }
 
+    @Transactional
     public ProjectResponse updateProject(Long id, ProjectRequest projectRequest, User user) {
         checkUser(id, user, "해당 프로젝트를 변경할 수 없습니다.");
         Project newProject = projectRepository.save(ProjectMapper.toEntity(projectRequest));
         return ProjectMapper.toDto(newProject);
     }
 
+    @Transactional
     public void deleteProject(Long id, User user) {
         checkUser(id, user, "해당 프로젝트를 삭제할 수 없습니다.");
         // 사용자의 프로젝트 중, 해당하는 프로젝트 삭제
