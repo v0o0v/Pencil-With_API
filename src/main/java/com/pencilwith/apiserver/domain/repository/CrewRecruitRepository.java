c9a42dac9efe0f860484426844db1c3ae27a42d6
v0o0v <v0o0v@daum.net>
2021-05-04 23:47:50 +0900

    - QueryDsl 도입

--- src/main/java/com/pencilwith/apiserver/domain/repository/CrewRecruitRepository.java
+++ src/main/java/com/pencilwith/apiserver/domain/repository/CrewRecruitRepository.java
@@ -3,8 +3,9 @@ package com.pencilwith.apiserver.domain.repository;
 import com.pencilwith.apiserver.domain.entity.CrewRecruit;
 import com.pencilwith.apiserver.domain.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface CrewRecruitRepository extends JpaRepository<CrewRecruit, Long> {
+public interface CrewRecruitRepository extends JpaRepository<CrewRecruit, Long>, QuerydslPredicateExecutor<CrewRecruit> {
 
     boolean existsByProject(Project p);
 
