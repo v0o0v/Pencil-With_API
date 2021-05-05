df98eea50b94f8f5e274d173ea7a182e345689a4
seculoper <mir8725@naver.com>
2021-05-05 11:41:46 +0900

    Project: Controller 응답 코드 추가 및 Swagger2 적용

--- src/main/java/com/pencilwith/apiserver/working/controller/ProjectController.java
+++ src/main/java/com/pencilwith/apiserver/working/controller/ProjectController.java
@@ -5,7 +5,10 @@ import com.pencilwith.apiserver.working.dto.project.ProjectRequest;
 import com.pencilwith.apiserver.working.dto.project.ProjectResponse;
 import com.pencilwith.apiserver.working.dto.project.ProjectTitleResponse;
 import com.pencilwith.apiserver.working.service.ProjectService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,33 +16,44 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/projects")
+@RequestMapping(value = "/projects", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProjectController {
     private final ProjectService projectService;
 
+    @ApiOperation(value = "소설 리스트 조회",
+                  notes = "My 소설의 전체 리스트를 조회합니다. id와 제목만 출력됩니다.")
     @GetMapping("/names")
-    public List<ProjectTitleResponse> getTitles(@AuthenticationPrincipal User user) {
-        return projectService.selectAllTitles(user);
+    public ResponseEntity<List<ProjectTitleResponse>> getTitles(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(projectService.selectAllTitles(user));
     }
 
+    @ApiOperation(value = "소설 한건 조회",
+            notes = "My 소설 한 건을 조회합니다.")
     @GetMapping("/{id}")
-    public ProjectResponse getProject(@RequestParam Long id, @AuthenticationPrincipal User user) {
-        return projectService.selectProject(id, user);
+    public ResponseEntity<ProjectResponse> getProject(@RequestParam Long id, @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(projectService.selectProject(id, user));
     }
 
+    @ApiOperation(value = "소설 발행",
+            notes = "입력받은 데이터를 기반으로 My 소설을 한 건 발행합니다.")
     @PostMapping
-    public ProjectResponse createProject(@RequestBody ProjectRequest projectRequest) {
-        return projectService.createProject(projectRequest);
+    public ResponseEntity<ProjectResponse> createProject(@RequestBody ProjectRequest projectRequest) {
+        return ResponseEntity.ok(projectService.createProject(projectRequest));
     }
 
+    @ApiOperation(value = "소설 수정",
+            notes = "입력받은 데이터를 기반으로 특정 My 소설을 수정합니다.")
     @PutMapping("/{id}")
-    public ProjectResponse updateProject(@RequestParam Long id, @RequestBody ProjectRequest projectRequest, @AuthenticationPrincipal User user) {
-        return projectService.updateProject(id, projectRequest, user);
+    public ResponseEntity<ProjectResponse> updateProject(@RequestParam Long id, @RequestBody ProjectRequest projectRequest, @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(projectService.updateProject(id, projectRequest, user));
     }
 
+    @ApiOperation(value = "소설 삭제",
+            notes = "특정 My 소설을 삭제합니다.")
     @DeleteMapping("/{id}")
-    public void deleteProject(@RequestParam Long id, @AuthenticationPrincipal User user) {
+    public ResponseEntity<?> deleteProject(@RequestParam Long id, @AuthenticationPrincipal User user) {
         projectService.deleteProject(id, user);
+        return ResponseEntity.ok().build();
     }
 
 }
