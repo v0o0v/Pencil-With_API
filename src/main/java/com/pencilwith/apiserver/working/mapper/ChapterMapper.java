fdd245d1eb4eef876253dc901be8d63cd38ae9ba
seculoper <mir8725@naver.com>
2021-05-05 11:38:36 +0900

    Project: Mapper 수정

--- src/main/java/com/pencilwith/apiserver/working/mapper/ChapterMapper.java
+++ src/main/java/com/pencilwith/apiserver/working/mapper/ChapterMapper.java
@@ -4,13 +4,21 @@ import com.pencilwith.apiserver.domain.entity.Chapter;
 import com.pencilwith.apiserver.working.dto.project.ChapterDto;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ChapterMapper {
-    public static List<ChapterDto> toDto(List<Chapter> chapters) {
+    public static List<ChapterDto> toDto(Set<Chapter> chapters) {
         return chapters.stream().map(chapter -> ChapterDto.builder()
                     .chapterId(chapter.getId())
                     .content(chapter.getContent())
         .build()).collect(Collectors.toList());
+    }
+
+    public static Set<Chapter> toEntity(List<ChapterDto> chapterDtoList) {
+        return chapterDtoList.stream().map(chapterDto -> Chapter.builder()
+                    .id(chapterDto.getChapterId())
+                    .content(chapterDto.getContent())
+        .build()).collect(Collectors.toSet());
     }
 }
--- src/main/java/com/pencilwith/apiserver/working/mapper/ProjectMapper.java
+++ src/main/java/com/pencilwith/apiserver/working/mapper/ProjectMapper.java
@@ -5,19 +5,28 @@ import com.pencilwith.apiserver.domain.entity.Project;
 import com.pencilwith.apiserver.working.dto.project.ProjectRequest;
 import com.pencilwith.apiserver.working.dto.project.ProjectResponse;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProjectMapper {
-    public static Project toEntity(ProjectRequest projectRequest) {
-        return new Project();
-    }
-
     public static ProjectResponse toDto(Project project) {
         return ProjectResponse.builder()
                 .ownerId(project.getOwner().getId())
                 .projectId(project.getId())
                 .title(project.getTitle())
-                .chapterList(ChapterMapper.toDto((List<Chapter>) project.getChapterList()))
+                .chapterList(ChapterMapper.toDto(new HashSet<>(project.getChapterList())))
+                .build();
+    }
+
+    public static Project toEntity(ProjectRequest projectRequest) {
+        return Project.builder()
+                .id(projectRequest.getProjectId())
+                .owner(projectRequest.getOwner())
+                .crewList(projectRequest.getCrewList())
+                .chapterList(ChapterMapper.toEntity(projectRequest.getChapterList()))
+                .title(projectRequest.getTitle())
                 .build();
     }
 }
