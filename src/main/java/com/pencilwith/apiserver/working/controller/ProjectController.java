fd62ffccd637ede5183169a81d711587129ad5a3
seculoper <mir8725@naver.com>
2021-05-06 21:42:49 +0900

    Project: User 정보 가져오는 방식 변경

--- src/main/java/com/pencilwith/apiserver/working/controller/ProjectController.java
+++ src/main/java/com/pencilwith/apiserver/working/controller/ProjectController.java
@@ -1,6 +1,5 @@
 package com.pencilwith.apiserver.working.controller;
 
-import com.pencilwith.apiserver.domain.entity.User;
 import com.pencilwith.apiserver.working.dto.project.ProjectRequest;
 import com.pencilwith.apiserver.working.dto.project.ProjectResponse;
 import com.pencilwith.apiserver.working.dto.project.ProjectTitleResponse;
@@ -9,29 +8,28 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/projects", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/projects", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProjectController {
     private final ProjectService projectService;
 
     @ApiOperation(value = "소설 리스트 조회",
                   notes = "My 소설의 전체 리스트를 조회합니다. id와 제목만 출력됩니다.")
     @GetMapping("/names")
-    public ResponseEntity<List<ProjectTitleResponse>> getTitles(@AuthenticationPrincipal User user) {
-        return ResponseEntity.ok(projectService.selectAllTitles(user));
+    public ResponseEntity<List<ProjectTitleResponse>> getTitles() {
+        return ResponseEntity.ok(projectService.selectAllTitles());
     }
 
     @ApiOperation(value = "소설 한건 조회",
             notes = "My 소설 한 건을 조회합니다.")
     @GetMapping("/{id}")
-    public ResponseEntity<ProjectResponse> getProject(@RequestParam Long id, @AuthenticationPrincipal User user) {
-        return ResponseEntity.ok(projectService.selectProject(id, user));
+    public ResponseEntity<ProjectResponse> getProject(@RequestParam Long id) {
+        return ResponseEntity.ok(projectService.selectProject(id));
     }
 
     @ApiOperation(value = "소설 발행",
@@ -44,15 +42,15 @@ public class ProjectController {
     @ApiOperation(value = "소설 수정",
             notes = "입력받은 데이터를 기반으로 특정 My 소설을 수정합니다.")
     @PutMapping("/{id}")
-    public ResponseEntity<ProjectResponse> updateProject(@RequestParam Long id, @RequestBody ProjectRequest projectRequest, @AuthenticationPrincipal User user) {
-        return ResponseEntity.ok(projectService.updateProject(id, projectRequest, user));
+    public ResponseEntity<ProjectResponse> updateProject(@RequestParam Long id, @RequestBody ProjectRequest projectRequest) {
+        return ResponseEntity.ok(projectService.updateProject(id, projectRequest));
     }
 
     @ApiOperation(value = "소설 삭제",
             notes = "특정 My 소설을 삭제합니다.")
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteProject(@RequestParam Long id, @AuthenticationPrincipal User user) {
-        projectService.deleteProject(id, user);
+    public ResponseEntity<?> deleteProject(@RequestParam Long id) {
+        projectService.deleteProject(id);
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/com/pencilwith/apiserver/working/service/ProjectService.java
+++ src/main/java/com/pencilwith/apiserver/working/service/ProjectService.java
@@ -4,34 +4,42 @@ import com.pencilwith.apiserver.domain.entity.Project;
 import com.pencilwith.apiserver.domain.entity.User;
 import com.pencilwith.apiserver.domain.exception.BadRequestException;
 import com.pencilwith.apiserver.domain.repository.ProjectRepository;
+import com.pencilwith.apiserver.domain.repository.UserRepository;
 import com.pencilwith.apiserver.working.dto.project.ProjectRequest;
 import com.pencilwith.apiserver.working.dto.project.ProjectResponse;
 import com.pencilwith.apiserver.working.dto.project.ProjectTitleResponse;
 import com.pencilwith.apiserver.working.mapper.ProjectMapper;
 import com.pencilwith.apiserver.working.mapper.ProjectTitleMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
-    public List<ProjectTitleResponse> selectAllTitles(User user) {
+    public List<ProjectTitleResponse> selectAllTitles() {
         // 현재 사용자의 프로젝트 리스트 조회
-        List<Project> projects = user.getProject();
+        User curUser = checkUser();
+
+        List<Project> projects = curUser.getOwnerProjectList();
+        if(projects.isEmpty())
+            throw new BadRequestException("해당 프로젝트들이 존재하지 않습니다.");
+
         return ProjectTitleMapper.toDto(projects);
     }
 
     @Transactional(readOnly = true)
-    public ProjectResponse selectProject(Long id, User user) {
+    public ProjectResponse selectProject(Long id) {
         // 현재 사용자의 프로젝트를 조회함
-        Project project = projectRepository.findProjectByOwnerId(user.getId());
+        Project project = checkProject(id, "해당 프로젝트를 조회할 수 없습니다.");
+
         return ProjectMapper.toDto(project);
     }
 
@@ -42,24 +50,35 @@ public class ProjectService {
     }
 
     @Transactional
-    public ProjectResponse updateProject(Long id, ProjectRequest projectRequest, User user) {
-        checkUser(id, user, "해당 프로젝트를 변경할 수 없습니다.");
+    public ProjectResponse updateProject(Long id, ProjectRequest projectRequest) {
+        checkProject(id, "해당 프로젝트를 변경할 수 없습니다.");
         Project newProject = projectRepository.save(ProjectMapper.toEntity(projectRequest));
         return ProjectMapper.toDto(newProject);
     }
 
     @Transactional
-    public void deleteProject(Long id, User user) {
-        checkUser(id, user, "해당 프로젝트를 삭제할 수 없습니다.");
+    public void deleteProject(Long id) {
+        checkProject(id, "해당 프로젝트를 삭제할 수 없습니다.");
         // 사용자의 프로젝트 중, 해당하는 프로젝트 삭제
         projectRepository.deleteById(id);
     }
 
-    private void checkUser(Long id, User user, String msg) {
+    private Project checkProject(Long id, String msg) {
+        User curUser = checkUser();
+
         Project project = projectRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException("해당 프로젝트가 존재하지 않습니다."));
+
         User Owner = project.getOwner();
-        if(Owner.getId().equals(user.getId()))
+        if(Owner.getId().equals(curUser.getId()))
             throw new BadRequestException(msg);
+
+        return project;
+    }
+
+    private User checkUser() {
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userRepository.findById(user.getUsername())
+                .orElseThrow(() -> new BadRequestException("존재하지 않는 사용자입니다."));
     }
 }
