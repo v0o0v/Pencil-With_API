8c0ac77d66c401f924497738bb35798dd11f2468
v0o0v <v0o0v@daum.net>
2021-05-08 22:24:17 +0900

    - AWS Service add. for upload file to S3.

---
+++ src/main/java/com/pencilwith/apiserver/util/AWSService.java
@@ -0,0 +1,60 @@
+package com.pencilwith.apiserver.util;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.pencilwith.apiserver.domain.exception.BadRequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class AWSService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile file) {
+        String fileName = createFileName(file.getOriginalFilename());
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(file.getContentType());
+
+        try (InputStream inputStream = file.getInputStream()) {
+            objectMetadata.setContentLength(file.getBytes().length);
+            upload(inputStream, objectMetadata, fileName);
+        } catch (IOException e) {
+            throw new BadRequestException(String.format("파일 변환 중 에러가 발생하였습니다 (%s)", file.getOriginalFilename()));
+        }
+        return getFileUrl(fileName);
+    }
+
+    public void upload(InputStream inputStream, ObjectMetadata objectMetadata, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public String getFileUrl(String fileName) {
+        return String.valueOf(amazonS3Client.getUrl(bucket, fileName));
+    }
+
+    private String createFileName(String originalFileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
+    }
+
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 (%s) 입니다", fileName));
+        }
+    }
+}
